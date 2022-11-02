dbb6b8680c34ffead38601eb90ead68996c658d8
Zbigniew Majchrowski <zmajchrowski@gmail.com>
2022-11-02 22:26:15 +0100

    build: inject app version as variable - REACT_APP_VERSION

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,21 @@
 const CracoAlias = require('craco-alias')
-
+const webpack = require('webpack')
 const dotenv = require('dotenv')
 const dotenvExpand = require('dotenv-expand')
 const config = dotenv.config()
 dotenvExpand.expand(config)
 
-module.exports = function () {
+const appVersion = process.env.npm_package_version
 
+module.exports = function () {
   return {
+    webpack: {
+      plugins: [
+         new webpack.DefinePlugin({
+            'process.env.REACT_APP_VERSION': JSON.stringify(appVersion)
+          }),
+      ],
+    },
     plugins: [
       {
         plugin: CracoAlias,
